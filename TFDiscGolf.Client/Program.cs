e64bf7cd3370c0bf0c45fce6a721bfdf05c57b5d
ColtMcG1 <coltmcg0@gmail.com>
2024-02-11 21:29:19 -0700

    Setup for Blazor Web App

--- TFDiscGolf.Client/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddBootstrapBlazor();
-
-builder.Services.AddTableDemoDataService();
-
-await builder.Build().RunAsync();
