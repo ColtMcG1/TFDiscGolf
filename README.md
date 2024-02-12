840116bf4dd69e1c90ab5b13f18027469a52d26f
ColtMcG1 <76269687+ColtMcG1@users.noreply.github.com>
2024-02-12 01:42:05 -0700

    Added authorization and SQL DB. Fixed navigation.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TF Disc Golf
 
-![Build status](https://github.com/ColtMcG1/TFDiscGolf/actions/workflows/dotnet.yml/badge.svg)
+![Build status](https://github.com/ColtMcG1/TFDiscGolf/actions/workflows/build.yml/badge.svg)
 
 This is the code that runs the TF Disc Golf website.
--- TFDiscGolf/TFDiscGolf/Program.cs
+++ TFDiscGolf/TFDiscGolf/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using TFDiscGolf.Client.Pages;
 using TFDiscGolf.Components;
+using TFDiscGolf.Components.Account;
+using TFDiscGolf.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +13,37 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+    app.UseMigrationsEndPoint();
 }
 else
 {
@@ -30,6 +60,9 @@ app.UseAntiforgery();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(TFDiscGolf.Client._Imports).Assembly);
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
 
 app.Run();
