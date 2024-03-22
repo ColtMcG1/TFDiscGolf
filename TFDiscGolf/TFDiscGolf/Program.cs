657d723411c13b70ece9b59f24be7d33f6302895
ColtMcG1 <76269687+ColtMcG1@users.noreply.github.com>
2024-03-22 00:43:41 -0600

    Production changes.

--- TFDiscGolf/TFDiscGolf/Program.cs
+++ TFDiscGolf/TFDiscGolf/Program.cs
@@ -50,16 +50,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
     app.UseMigrationsEndPoint();
+    app.UseHttpsRedirection();
+    app.UseHsts();
 }
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseForwardedHeaders();
 }
-
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
